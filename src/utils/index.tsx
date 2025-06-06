f28c850c11ac8e5834d51524f66122501d9a5674
Max Hawkins <maximuskwame@gmail.com>
2025-06-06 10:58:06 -0700

    change name to randomError

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,4 +1,4 @@
-export const randomErrorString = (percentChance: number = 0.3) => {
+export const randomError = (percentChance: number = 0.3) => {
   if (Math.random() <= percentChance) {
     throw new Error("Simulated API error");
   }
