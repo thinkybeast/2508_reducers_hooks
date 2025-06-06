e452bc0984943de5635bc33f7b4f0882e04d0b55
Max Hawkins <maximuskwame@gmail.com>
2025-06-06 10:01:37 -0700

    Update User component to use fakerapi.it and robohash.org

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import z from "zod";
 
 const userSchema = z.object({
-  avatar: z.string(),
-  first_name: z.string(),
-  employment: z.object({
-    key_skill: z.string(),
-  }),
+  data: z.array(
+    z.object({
+      firstname: z.string(),
+      email: z.string(),
+      website: z.string(),
+    })
+  ),
 });
 
-type User = z.infer<typeof userSchema>;
+type UserResponse = z.infer<typeof userSchema>;
 
 const User = () => {
-  const [user, setUser] = React.useState<User | null>(null);
+  const [user, setUser] = React.useState<UserResponse["data"][0] | null>(null);
 
   async function fetchUser() {
     try {
@@ -20,12 +22,14 @@ const User = () => {
       await new Promise<void>((resolve) => setTimeout(() => resolve(), 1500));
 
       // Fetch user data from API
-      const result = await fetch("https://random-data-api.com/api/v2/users");
+      const result = await fetch(
+        "https://fakerapi.it/api/v2/users?_quantity=1"
+      );
       const userData = await result.json();
 
       // Validate API response
       userSchema.parse(userData);
-      setUser(userData);
+      setUser(userData.data[0]);
     } catch (error) {
       console.error(error);
     }
@@ -41,12 +45,15 @@ const User = () => {
       {user ? (
         <div>
           <div style={{ width: "310px", height: "310px", margin: "0 auto" }}>
-            <img src={user.avatar} key={user.avatar} />
+            <img
+              src={`https://robohash.org/${user.email}`}
+              key={`https://robohash.org/${user.email}`}
+            />
           </div>
           <p>
-            Meet <b>{user.first_name}!</b>
+            Meet <b>{user.firstname}!</b>
           </p>
-          <p>They are passionate about {user.employment.key_skill}</p>
+          <p>They are passionate about {user.website}</p>
           <button onClick={fetchUser}>Not cool enough. Give me another.</button>
         </div>
       ) : null}
