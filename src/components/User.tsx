9a9d9efb19f2795dbda16f8f7761e7f5d45d1386
Max Hawkins <maximuskwame@gmail.com>
2025-06-06 10:03:27 -0700

    Update User component to use fakerapi.it and robohash.org

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -4,17 +4,19 @@ import Loading from "./Loading";
 import { randomErrorString } from "@/utils";
 
 const userSchema = z.object({
-  avatar: z.string(),
-  first_name: z.string(),
-  employment: z.object({
-    key_skill: z.string(),
-  }),
+  data: z.array(
+    z.object({
+      firstname: z.string(),
+      email: z.string(),
+      website: z.string(),
+    })
+  ),
 });
 
-type User = z.infer<typeof userSchema>;
+type UserResponse = z.infer<typeof userSchema>;
 
 const User = () => {
-  const [user, setUser] = React.useState<User | null>(null);
+  const [user, setUser] = React.useState<UserResponse["data"][0] | null>(null);
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
   async function fetchUser() {
@@ -27,13 +29,13 @@ const User = () => {
       // Fetch user data from API
       // Randomly add either an empty string or an nonsense string to the URL to simulate an API error
       const result = await fetch(
-        "https://random-data-api.com/api/v2/users" + randomErrorString()
+        "https://fakerapi.it/api/v2/users?_quantity=1" + randomErrorString()
       );
       const userData = await result.json();
 
       // Validate API response
       userSchema.parse(userData);
-      setUser(userData);
+      setUser(userData.data[0]);
     } catch (error) {
       console.error(error);
     } finally {
@@ -55,12 +57,15 @@ const User = () => {
       {user ? (
         <div>
           <div style={{ width: "310px", height: "310px", margin: "0 auto" }}>
-            <img src={user.avatar} key={user.avatar} />
+            <img
+              src={`https://robohash.org/${user.email}`}
+              key={`https://robohash.org/${user.email}`}
+            />
           </div>
           <p>
-            Meet <b>{user.first_name}!</b>
+            Meet <b>{user.firstname}!</b>
           </p>
-          <p>They are passionate about {user.employment.key_skill}</p>
+          <p>They are passionate about {user.website}</p>
           <button onClick={fetchUser}>Not cool enough. Give me another.</button>
         </div>
       ) : null}
