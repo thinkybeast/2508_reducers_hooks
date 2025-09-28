4db657f845ebc2b5d1eff962b61831312c8ffe5b
Max Hawkins <maximuskwame@gmail.com>
2025-09-28 16:01:05 -0700

    chore: remove deprecated Book components and directory

--- src/components/Book/BookLoading.tsx
+++
@@ -1,9 +0,0 @@
-const Loading = () => {
-  return (
-    <h2 style={{ color: "orchid", fontStyle: "italic" }}>
-      Finding the most interesting of books...
-    </h2>
-  );
-};
-
-export default Loading;
