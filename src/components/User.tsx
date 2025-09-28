abb25746ad67b868490d4d73cee49796d0b1cacf
Max Hawkins <maximuskwame@gmail.com>
2025-09-28 15:12:42 -0700

    feat(user): migrate to cool-fake-data API, keep loading UI, update copy (branch 4)

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -3,19 +3,17 @@ import z from "zod";
 import Loading from "./Loading";
 
 const userSchema = z.object({
-  data: z.array(
-    z.object({
-      firstname: z.string(),
-      email: z.string(),
-      website: z.string(),
-    })
-  ),
+  firstName: z.string(),
+  fullName: z.string(),
+  jobTitle: z.string(),
+  bio: z.string(),
+  avatar: z.string(),
 });
 
-type UserResponse = z.infer<typeof userSchema>;
+type User = z.infer<typeof userSchema>;
 
 const User = () => {
-  const [user, setUser] = React.useState<UserResponse["data"][0] | null>(null);
+  const [user, setUser] = React.useState<User | null>(null);
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
   async function fetchUser() {
@@ -25,15 +23,15 @@ const User = () => {
       // Simulate a delay
       await new Promise<void>((resolve) => setTimeout(() => resolve(), 1500));
 
-      // Fetch user data from API
+      // Fetch user data from new API
       const result = await fetch(
-        "https://fakerapi.it/api/v2/users?_quantity=1"
+        "https://cool-fake-data.up.railway.app/api/user"
       );
       const userData = await result.json();
 
       // Validate API response
       userSchema.parse(userData);
-      setUser(userData.data[0]);
+      setUser(userData);
     } catch (error) {
       console.error(error);
     } finally {
@@ -55,15 +53,14 @@ const User = () => {
       {user ? (
         <div>
           <div style={{ width: "310px", height: "310px", margin: "0 auto" }}>
-            <img
-              src={`https://robohash.org/${user.email}`}
-              key={`https://robohash.org/${user.email}`}
-            />
+            <img src={user.avatar} alt={user.fullName} />
           </div>
           <p>
-            Meet <b>{user.firstname}!</b>
+            Meet <b>{user.firstName}!</b> They are a <b>{user.jobTitle}</b>.
           </p>
-          <p>Check out their work at {user.website}!</p>
+          <p>
+            <b>Key facts:</b> {user.bio}
+          </p>
           <button onClick={fetchUser}>Not cool enough. Give me another.</button>
         </div>
       ) : null}
